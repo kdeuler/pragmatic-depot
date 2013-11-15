5023361db1ca5a62f8ff1658fb83e8a3c799447c
Kurt Euler <kdeuler@gmail.com>
2013-11-14 20:55:29 -0800

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url    'lorem.jpg',
+      price:       19.95
+    }
   end
 
   test "should get index" do
@@ -18,6 +24,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
+    # Book page 81 is different. I'm leaving it way I found it.
       post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     end
 
@@ -35,6 +42,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
+    # Book page 81 is different. I'm leaving it way I found it.
     patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     assert_redirected_to product_path(assigns(:product))
   end
